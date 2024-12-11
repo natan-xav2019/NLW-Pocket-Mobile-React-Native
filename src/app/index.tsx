a486408eae7995aae88ee816840f5fae6a25bf55
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2024-12-11 10:30:26 -0300

    Feat: Create component Welcome

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+import { Welcome } from '@/components/welcome'
+
+export default function Index() {
+    return (
+        <View style={{ flex: 1, padding: 40, gap: 40 }}>
+            <Welcome />
+        </View>
+    )
+}
